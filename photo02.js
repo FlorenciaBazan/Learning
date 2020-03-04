f5131666697d78f26a2d03dcfab01134a9af0f29
Florencia Bazan <florenciacbazan@gmail.com>
2020-03-04 16:53:21 -0300

    Worked with listener to change background colour.

--- photo02.js
+++ photo02.js
@@ -3,7 +3,23 @@ window.onload = function() {
   //Listener
   photos.addEventListener("click",function(e){
     if (e.target.tagName === "IMG"){
-      console.log("I pressed an image");
+      var blackBackground = document.createElement("div");
+      blackBackground.id = "overlay";
+      document.body.appendChild(blackBackground);
+
+      //Give style to the background
+      blackBackground.style.position = "absolute";
+      blackBackground.style.top = 0;
+      blackBackground.style.background= "rgba(0,0,0,0.5)";
+      blackBackground.style.cursor = "pointer";
+
+      //
+      blackBackground.style.width = window.innerWidth+"px";
+      blackBackground.style.height = window.innerHeight+"px";
+      blackBackground.style.top = window.pageYOffset+"px";
+      blackBackground.style.left = window.pageXOffset+"px";
+
+
     } else {
       console.log("I didn't press an image");
     }
