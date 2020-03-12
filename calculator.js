def4ef1723b3351b3da68b067cbd66e19b60eaa3
Florencia Bazan <florenciacbazan@gmail.com>
2020-03-12 11:38:30 -0300

    Create calculator buttons

--- calculator.js
+++ calculator.js
@@ -1,6 +1,25 @@
 window.onload = function(){
+  var signs = "+-*/";
+  var x = new Array();
   document.getElementById("value").onkeypress = function(event){
     return numbers(event);
+  }
+  x = document.querySelectorAll("input[type=button]");
+  for (var i = 0; i < x.length; i++){
+    x[i].onclick = function(){
+      let n = this.value;
+      if (n=="C"){
+        deleteF();
+      } else if(n=="<"){
+        deleteCharacter();
+      } else if (n=="=") {
+        calculate();
+      } else if (signs.indexOf(n)>-1){
+        validateSign(n);
+      } else {
+        back(n);
+      }
+    }
   }
 }
 
@@ -19,5 +38,19 @@ function numbers(e){
   if(numbers.indexOf(keyboard)==-1 && flag==false){
     return false;
   }
-
+}
+function deleteF(){
+  console.log("delete");
+}
+function deleteCharacter(){
+  console.log("delete character");
+}
+function calculate(){
+  console.log("calculate")
+}
+function validateSign(n){
+  console.log("validateSign");
+}
+function back(n){
+  console.log("return number");
 }
